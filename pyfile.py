5bb3064f16a4f82ed6a6d7160d02ef6e6023a81e
Eduardo Mendes <eduardo.mendes@engenharia.ufjf.br>
2023-08-14 14:44:53 -0300

    arquivos modificados

--- pyfile.py
+++ pyfile.py
@@ -1,7 +1,10 @@
 def fibonacci(n):
-    if n == 0:
-        return 0
-    elif n == 1:
-        return 1
+    if n <= 1:
+        return n
     else:
         return fibonacci(n-1) + fibonacci(n-2)
+    
+
+# Rodar Fibonacci
+
+fibonacci(10)
